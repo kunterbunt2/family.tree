aa1f891d64b9a4cc62e25f54f3375fb9cd26c1dc
kunterbunt2 <abdalla@bushnaq.de>
2022-09-02 15:57:28 +0200

    First version with installler

---
+++ mvn_install_skipTest.bat
@@ -0,0 +1,8 @@
+REM
+REM compile and package the project without running the tests
+REM
+
+chcp 65001
+rem call mvn clean verify spring-boot:repackage -DskipTests -X
+call mvn clean install -DskipTests
+pause
